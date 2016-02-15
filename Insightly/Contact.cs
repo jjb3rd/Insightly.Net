72a475c4bf55b1cf8d9f76d18b45c3ba71a4f90a
elliotchaim <elliot.chaim@razorjam.co.uk>
2016-02-15 10:46:10 +0000

    fleshed out the Contact class

--- Insightly/Contact.cs
+++ Insightly/Contact.cs
@@ -58,10 +58,55 @@ namespace Insightly
     [JsonProperty(PropertyName = "LAST_NAME", NullValueHandling = NullValueHandling.Ignore)]
     public string LastName { get; set; }
 
+    [JsonProperty(PropertyName = "BACKGROUND", NullValueHandling = NullValueHandling.Ignore)]
+    public string Background { get; set; }
+
+    [JsonProperty(PropertyName = "IMAGE_URL", NullValueHandling = NullValueHandling.Ignore)]
+    public string ImageUrl { get; set; }
+
+    [JsonProperty(PropertyName = "DEFAULT_LINKED_ORGANISATION", NullValueHandling = NullValueHandling.Ignore)]
+    public int? DefaultLinkedOrganisation { get; set; }
+
+    [JsonProperty(PropertyName = "OWNER_USER_ID", NullValueHandling = NullValueHandling.Ignore)]
+    public int? OwnerUserId { get; set; }
+
+    [JsonProperty(PropertyName = "DATE_CREATED_UTC", NullValueHandling = NullValueHandling.Ignore)]
+    public string DateCreated { get; set; }
+
+    [JsonProperty(PropertyName = "DATE_UPDATED_UTC", NullValueHandling = NullValueHandling.Ignore)]
+    public string DateUpdated { get; set; }
+
+    [JsonProperty(PropertyName = "VISIBLE_TO", NullValueHandling = NullValueHandling.Ignore)]
+    public string VisibleTo { get; set; }
+
+    [JsonProperty(PropertyName = "VISIBLE_TEAM_ID", NullValueHandling = NullValueHandling.Ignore)]
+    public int? VisibleTeamId { get; set; }
+
+    [JsonProperty(PropertyName = "VISIBLE_USER_IDS", NullValueHandling = NullValueHandling.Ignore)]
+    public string VisibleUserIds { get; set; }
+
+    [JsonProperty(PropertyName = "CUSTOMFIELDS", NullValueHandling = NullValueHandling.Ignore)]
+    public IEnumerable<CustomField> CustomFields { get; set; }
+
+    [JsonProperty(PropertyName = "ADDRESSES", NullValueHandling = NullValueHandling.Ignore)]
+    public IEnumerable<Address> Addresses { get; set; }
+
     [JsonProperty(PropertyName = "CONTACTINFOS", NullValueHandling = NullValueHandling.Ignore)]
     public IEnumerable<ContactInfo> ContactInfos { get; set; }
 
+    [JsonProperty(PropertyName = "DATES", NullValueHandling = NullValueHandling.Ignore)]
+    public IEnumerable<Date> Dates { get; set; }
+
+    [JsonProperty(PropertyName = "TAGS", NullValueHandling = NullValueHandling.Ignore)]
+    public IEnumerable<Tag> Tags { get; set; }
+
     [JsonProperty(PropertyName = "LINKS", NullValueHandling = NullValueHandling.Ignore)]
     public IEnumerable<Link> Links { get; set; }
+
+    [JsonProperty(PropertyName = "CONTACTLINKS", NullValueHandling = NullValueHandling.Ignore)]
+    public IEnumerable<ContactLink> ContactLinks { get; set; }
+
+    [JsonProperty(PropertyName = "EMAILLINKS", NullValueHandling = NullValueHandling.Ignore)]
+    public IEnumerable<EmailLink> EmailLinks { get; set; }
   }
 }
