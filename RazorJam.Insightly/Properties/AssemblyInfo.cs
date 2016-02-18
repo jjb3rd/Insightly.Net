88881a3e6276363ac2ede112d0a2d77e77e5a287
elliotchaim <elliot.chaim@razorjam.co.uk>
2016-02-18 16:10:43 +0000

    Created nuget package

---
+++ RazorJam.Insightly/Properties/AssemblyInfo.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Started by Beckersoft, Inc.
+ * Extended by Razor Jam (razorjam.co.uk)
+ *
+ * Author(s):
+ *  John Becker (john@beckersoft.com)
+ *  Elliot Chaim (elliot.chaim@razorjam.co.uk)
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Insightly.Net")]
+[assembly: AssemblyDescription("Insightly v2.1 REST API .NET wrapper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Razor Jam")]
+[assembly: AssemblyProduct("RazorJam.Insightly")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: System.CLSCompliant(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d8507a3-65c3-48ed-b6a7-8beec28d6783")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
---
+++ RazorJam.Insightly/TaskLink.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Started by Beckersoft, Inc.
+ * Extended by Razor Jam (razorjam.co.uk)
+ *
+ * Author(s):
+ *  John Becker (john@beckersoft.com)
+ *  Elliot Chaim (elliot.chaim@razorjam.co.uk)
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Newtonsoft.Json;
+
+namespace RazorJam.Insightly
+{
+  [JsonObject(MemberSerialization.OptIn)]
+  public class TaskLink
+  {
+    [JsonProperty(PropertyName = "TASK_LINK_ID")]
+    public int TaskLinkId { get; set; }
+
+    [JsonProperty(PropertyName = "TASK_ID")]
+    public int TaskId { get; set; }
+
+    [JsonProperty(PropertyName = "CONTACT_ID")]
+    public int ContactId { get; set; }
+
+    [JsonProperty(PropertyName = "ORGANISATION_ID")]
+    public int OrganizationId { get; set; }
+
+    [JsonProperty(PropertyName = "OPPORTUNITY_ID")]
+    public int OpportunityId { get; set; }
+
+    [JsonProperty(PropertyName = "PROJECT_ID")]
+    public int ProjectId { get; set; }
+  }
+}
