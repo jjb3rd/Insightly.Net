aad4948b5c7f922131776532616a6b5a74295394
elliotchaim <elliot.chaim@razorjam.co.uk>
2016-02-29 09:57:22 +0000

    changed nuget ownership

--- RazorJam.Insightly/Properties/AssemblyInfo.cs
+++ RazorJam.Insightly/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Insightly.Net")]
-[assembly: AssemblyDescription("Insightly v2.1 REST API .NET wrapper")]
+[assembly: AssemblyDescription("RazorJam.Insightly.Net is a fluent, cached wrapper service for the Insightly REST-based API v2.1.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Razor Jam")]
 [assembly: AssemblyProduct("RazorJam.Insightly")]
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
